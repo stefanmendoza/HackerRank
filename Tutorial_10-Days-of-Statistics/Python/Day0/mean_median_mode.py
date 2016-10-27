c1ffe986f644f5041eb2f0f506f670a3fc234dbb
Stefan Mendoza <stefanmendoza.dev@gmail.com>
2016-10-27 01:07:05 -0500

    Added Python solutions for Day0

---
+++ Tutorial_10-Days-of-Statistics/Python/Day0/mean_median_mode.py
@@ -0,0 +1,30 @@
+# Day 0 - Mean, Median & Mode
+#
+# Author: Stefan Mendoza
+# Date Created: 27 October 2016
+
+from math import ceil, floor
+import sys
+
+values = list(map(int, sys.stdin.readlines()[1].split(' ')))
+
+# Mean
+print('{0:.1f}'.format(sum(values)/len(values)))
+
+# Median
+values.sort()
+
+if len(values) % 2 != 0:
+    print('{0:.1f}'.format(values[len(values)/2]))
+else:
+    first_median_value = int(floor(len(values)/2)) - 1
+    second_median_value = int(ceil(len(values)/2))
+    print('{0:.1f}'.format((values[first_median_value] + values[second_median_value])/2.0))
+
+# Mode
+modes = [(x, values.count(x)) for x in values if values.count(x) > 1]
+
+if len(modes) == 0:
+    print(min(values))
+else:
+    print(modes[0][0])
---
+++ Tutorial_10-Days-of-Statistics/Python/Day0/weighted_mean.py
@@ -0,0 +1,13 @@
+# Day 0 - Weighted Mean
+#
+# Author: Stefan Mendoza
+# Date Created: 27 October 2016
+
+from math import ceil, floor
+import sys
+
+stdin = sys.stdin.readlines()
+x = list(map(int, stdin[1].strip().split(' ')))
+w = list(map(int, stdin[2].strip().split(' ')))
+
+print('{0:.1f}'.format(sum([a * b for (a,b) in zip(x,w)]) / sum(w)))
