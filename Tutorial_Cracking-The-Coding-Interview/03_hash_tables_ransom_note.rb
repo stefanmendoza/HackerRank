bb4dcabde6de230714fa33f5c612287e447df1d7
Stefan Mendoza <stefanmendoza.dev@gmail.com>
2016-10-28 01:12:37 -0500

    Added Problem 3 for HackerRank Cracking the Coding Interview tutorial

---
+++ Tutorial_Cracking-The-Coding-Interview/03_hash_tables_ransom_note.rb
@@ -0,0 +1,40 @@
+#!/bin/ruby
+
+m,n = gets.strip.split(' ')
+m = m.to_i
+n = n.to_i
+magazine = gets.strip
+magazine = magazine.split(' ')
+ransom = gets.strip
+ransom = ransom.split(' ')
+
+h = Hash.new
+
+magazine.each do |word|
+  if h[word] == nil then
+    h[word] = 1
+  else
+    h[word] = h[word] + 1
+  end
+end
+
+works = true
+
+ransom.each do |word|
+  if h[word] == nil then
+    works = false
+    break
+  else
+    h[word] = h[word] - 1
+  end
+
+  if h[word] == 0 then
+    h.delete(word)
+  end
+end
+
+if works then
+  print 'Yes'
+else
+  print 'No'
+end
