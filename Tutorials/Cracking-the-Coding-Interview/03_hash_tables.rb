6ccec11a8c4408d86c773acf6093e452462e10e9
Stefan Mendoza <stefanmendoza.dev@gmail.com>
2016-10-30 18:13:12 -0500

    Cleaned up code for CTCI - Ruby - Hash Tables

--- Tutorials/Cracking-the-Coding-Interview/03_hash_tables.rb
+++ Tutorials/Cracking-the-Coding-Interview/03_hash_tables.rb
@@ -10,18 +10,18 @@
 # == Source
 # https://www.hackerrank.com/challenges/ctci-ransom-note
 
-m,n = gets.strip.split(' ')
+m,n = gets.strip.split
 m = m.to_i
 n = n.to_i
 magazine = gets.strip
-magazine = magazine.split(' ')
+magazine = magazine.split
 ransom = gets.strip
-ransom = ransom.split(' ')
+ransom = ransom.split
 
 h = Hash.new
 
 magazine.each do |word|
-  if h[word] == nil then
+  if h[word].nil?
     h[word] = 1
   else
     h[word] = h[word] + 1
@@ -31,20 +31,16 @@ end
 works = true
 
 ransom.each do |word|
-  if h[word] == nil then
+  if h[word].nil?
     works = false
     break
   else
     h[word] = h[word] - 1
   end
 
-  if h[word] == 0 then
+  if h[word] == 0
     h.delete(word)
   end
 end
 
-if works then
-  print 'Yes'
-else
-  print 'No'
-end
+if works then print 'Yes' else print 'No' end
